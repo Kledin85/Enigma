d0ceb66050300209f3c13e0da5d12d7bc36dad93
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-11 10:45:43 -0600

    changed enigma test and lib to follow interaction pattern.

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -3,7 +3,7 @@ require './lib/enigma'
 RSpec.describe Enigma do
 
   it 'exist' do
-    enigma = Enigma.new("hello world", "12345", "111122")
+    enigma = Enigma.new
 
     expect(enigma).to be_a(Enigma)
   end
