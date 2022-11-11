3a4d9224a1c69eafb0d0672cd5779f216bc4bd39
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-11 10:59:15 -0600

    Write a test to see if the encryption class has a character set

--- spec/encryption_spec.rb
+++ spec/encryption_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe Encryption do
 
     expect(encryption).to be_a(Encryption)
   end
+
+  it 'has a character set' do
+    encryption = Encryption.new
+
+    expect(encryption.character_set.first).to eq(["a"])
+    expect(encryption.character_set.last).to eq ([" "])
+  end
 end
