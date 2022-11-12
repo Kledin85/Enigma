5716c29530637245e9cb9db89f1d5f148c75fa19
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-12 13:03:26 -0600

    Have the shifts method passing

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -25,34 +25,55 @@ class Enigma
     "#{Time.now.strftime("%m%d%y")}"
   end
 
-  def date_sq
-    date.to_i ** 2
+  def date_sq(date_key = date)
+    date_key.to_i ** 2
   end
 
-  def offset_key
-    date_sq.digits.reverse.last(4).join
+  def offset_key(date_key = date)
+    date_sq(date_key).digits.reverse.last(4).join
+    #comeback to look at why its reversing
   end
 
-  def offset
+  def offset(date_key = date)
     {
-      :offset_a => offset_key[0].to_i,
-      :offset_b => offset_key[1].to_i,
-      :offset_c => offset_key[2].to_i,
-      :offset_d => offset_key[3].to_i
+      :offset_a => offset_key(date_key)[0].to_i,
+      :offset_b => offset_key(date_key)[1].to_i,
+      :offset_c => offset_key(date_key)[2].to_i,
+      :offset_d => offset_key(date_key)[3].to_i
     }
   end
 
-  def final_shift(key_arg = num_gen)
+  def final_shift(key_arg = num_gen, date_key = date)
+    # require 'pry' ; binding.pry
     {
-      :shift_a =>  keys(key_arg).values[0] + offset.values[0],
-      :shift_b =>  keys(key_arg).values[1] + offset.values[1],
-      :shift_c =>  keys(key_arg).values[2] + offset.values[2],
-      :shift_d =>  keys(key_arg).values[3] + offset.values[3]
+      :shift_a =>  keys(key_arg).values[0] + offset(date_key).values[0],
+      :shift_b =>  keys(key_arg).values[1] + offset(date_key).values[1],
+      :shift_c =>  keys(key_arg).values[2] + offset(date_key).values[2],
+      :shift_d =>  keys(key_arg).values[3] + offset(date_key).values[3]
     }
   end
 
-  # def encrypt(message, key = num_gen, date)
-  #   hash = {}
-  #   encrypted = message
-  # end
+  def shifts(message, key = num_gen, date)
+    shifts = final_shift(key, date).values
+    (0..message.length).each_with_object([]) do |num, array|
+      array.concat(shifts) if (num % 4).zero?
+    end[0...message.length]
+  end
+
+def encrypt(message, key = num_gen, date)
+  require 'pry' ; binding.pry
+  message.chars.map_with_index do |val, index|
+  end
+
+    # split_characters = message.split("")
+    #  new = split_characters.each do |chars|
+    #   character_set.rotate(final_shift(key, date)[:shift_a] + character_set.index(chars))[0]
+    #  end
+
+    # hash = {}
+    # encrypted = message
+  end
+
+
+
 end
